a601f317e997cf2b50e8b88f456fa494c97ed1c3
Karl Amoguis <ktamoguis@gmail.com>
2018-06-01 00:48:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, cast(pledges.amount as integer)
   FROM projects
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON projects.id = pledges.project_id
   WHERE projects.category = 'music';"
 end
